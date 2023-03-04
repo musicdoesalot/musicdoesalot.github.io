2d6cc1d1b7cd3fa29069a4382e05683fc655f77f
Jeremy <104031828+musicdoesalot@users.noreply.github.com>
2023-03-04 12:28:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const buttons = document.querySelectorAll(".playButton");
 const audios = document.querySelectorAll("audio");
 
-buttons.forEach((button, buttonIndex) => {
-  button.addEventListener("click", () => {
-    buttons.forEach((button) => button.classList.remove("blue"));
+buttons.forEach(function playButton(button, index) {
+    button.addEventListener('click', function () {
+        buttons.forEach((button) => button.classList.remove("blue"));
 
-    audios.forEach((audio, audioIndex) => {
-      if (buttonIndex !== audioIndex || audio.currentTime > 0) {
-        audio.src = "";
-      } else {
-        button.classList.add("blue")
-        audio.src = button.dataset.src;
-        audio.play();
-      }
+        audios.forEach(function audio(audio, audioIndex) {
+            if (buttonIndex !== audioIndex || audio.currentTime > 0) {
+                audio.src = "";
+            } else {
+                button.classList.add("blue")
+                audio.src = button.dataset.src;
+                audio.play();
+            }
+        });
     });
-  });
 });
-
