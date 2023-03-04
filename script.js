f6cd764f7d7d7660ab8904aec2b5dd08241d256a
Jeremy <104031828+musicdoesalot@users.noreply.github.com>
2023-03-04 13:41:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const buttons = document.querySelectorAll(".playButton");
 const audios = document.querySelectorAll("audio");
 
-buttons.forEach(function playButton(button, index) {
+buttons.forEach(function playButton(button, buttonIndex) {
     button.addEventListener("click", function () {
-        buttons.forEach((button) => button.classList.remove("blue"));
-
+        buttons.forEach(function (button) {
+            button.classList.remove("blue");
+        });
         audios.forEach(function audio(audio, audioIndex) {
             if (buttonIndex !== audioIndex || audio.currentTime > 0) {
                 audio.src = "";
