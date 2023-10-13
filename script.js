029b3729558e7eb2c150cff9a88d8d4fecc992a1
Jeremy <104031828+musicdoesalot@users.noreply.github.com>
2023-10-12 21:38:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,40 @@
-function randomBackground() {
-    const classNames = [
-        "bg1",
-        "bg2",
-        "bg3",
-        "bg4",
-        "bg5",
-        "bg6",
-        "bg7",
-        "bg8",
-        "bg9"
-    ];
+const tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    const randomIndex = Math.floor(Math.random() * classNames.length);
-    document.querySelector("body").classList.add(classNames[randomIndex]);
+let player;
+
+function onPlayerReady(event) {
+    player = event.target;
+    player.setVolume(100);
+    shufflePlaylist(player);
 }
 
-randomBackground();
+// Shuffle the playlist
+function shufflePlaylist(player) {
+    player.setShuffle(true);
+    player.playVideoAt(0);
+    player.stopVideo();
+}
 
-const videoPlayer = (function makeVideoPlayer() {
-    let player;
-
-    function loadIframeScript() {
-        const tag = document.createElement("script");
-        tag.src = "https://www.youtube.com/iframe_api";
-        const firstScriptTag = document.getElementsByTagName("script")[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    }
-
-    function onYouTubeIframeAPIReady() {
-        const frameContainer = document.querySelector(".video");
-        addPlayer(frameContainer);
-    }
-
-    function shufflePlaylist(player) {
-        player.setShuffle(true);
-        player.playVideoAt(0);
-        player.stopVideo();
-    }
-
-    function onPlayerReady(event) {
-        player = event.target;
-        player.setVolume(100);
-        shufflePlaylist(player);
-    }
-
-    function addPlayer(video) {
-        const options = {
-            height: 360,
-            host: "https://www.youtube-nocookie.com",
-            width: 642
-        };
-        options.playerVars = {
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '361',
+        host: 'https://www.youtube-nocookie.com',
+        width: '642',
+        playerVars: {
             autoplay: 0,
             cc_load_policy: 0,
             controls: 1,
             disablekb: 1,
             fs: 0,
             iv_load_policy: 3,
-            //loop:1,
             listType: 'playlist',
-            list: 'PLlotB_y9MoPlvAJM52qOYh9FA8VonzVki'
-        };
-        options.events = {
-            "onReady": onPlayerReady
-        };
-
-        player = new YT.Player(video, options);
-    }
-
-    function init() {
-        player = null;
-        loadIframeScript();
-        window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-    }
-
-    return {
-        init
-    };
-}());
-videoPlayer.init();
+            list: 'PLhBgTdAWkxeCMHYCQ0uuLyhydRJGDRNo5'
+        },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
